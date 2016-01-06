9231aa9f750e8a4812d160cae4860a1aee056fb0
Nathan Youngman <git@nathany.com>
2016-01-06 13:53:55 -0700

    payload: table driven tests

--- payload/aps_test.go
+++ payload/aps_test.go
@@ -7,26 +7,43 @@ import (
 	"github.com/RobotsAndPencils/buford/payload/badge"
 )
 
-func TestSimpleAlert(t *testing.T) {
-	p := payload.APS{Alert: payload.Alert{Body: "Message received from Bob"}}
-	expected := []byte(`{"aps":{"alert":"Message received from Bob"}}`)
-	testPayload(t, p, expected)
-}
-
-func TestBadgeAndSound(t *testing.T) {
-	p := payload.APS{
-		Alert: payload.Alert{Body: "You got your emails."},
-		Badge: badge.New(9),
-		Sound: "bingbong.aiff",
+func TestPayload(t *testing.T) {
+	var tests = []struct {
+		input    payload.APS
+		expected []byte
+	}{
+		{
+			payload.APS{
+				Alert: payload.Alert{Body: "Message received from Bob"},
+			},
+			[]byte(`{"aps":{"alert":"Message received from Bob"}}`),
+		},
+		{
+			payload.APS{
+				Alert: payload.Alert{Body: "You got your emails."},
+				Badge: badge.New(9),
+				Sound: "bingbong.aiff",
+			},
+			[]byte(`{"aps":{"alert":"You got your emails.","badge":9,"sound":"bingbong.aiff"}}`),
+		},
+		{
+			payload.APS{ContentAvailable: true},
+			[]byte(`{"aps":{"content-available":1}}`),
+		},
+		{
+			payload.APS{
+				Alert: payload.Alert{
+					Title: "Message",
+					Body:  "Message received from Bob",
+				},
+			},
+			[]byte(`{"aps":{"alert":{"title":"Message","body":"Message received from Bob"}}}`),
+		},
 	}
-	expected := []byte(`{"aps":{"alert":"You got your emails.","badge":9,"sound":"bingbong.aiff"}}`)
-	testPayload(t, p, expected)
-}
 
-func TestContentAvailable(t *testing.T) {
-	p := payload.APS{ContentAvailable: true}
-	expected := []byte(`{"aps":{"content-available":1}}`)
-	testPayload(t, p, expected)
+	for _, tt := range tests {
+		testPayload(t, tt.input, tt.expected)
+	}
 }
 
 func TestCustomArray(t *testing.T) {
@@ -35,10 +52,4 @@ func TestCustomArray(t *testing.T) {
 	pm["acme2"] = []string{"bang", "whiz"}
 	expected := []byte(`{"acme2":["bang","whiz"],"aps":{"alert":"Message received from Bob"}}`)
 	testPayload(t, pm, expected)
-}
-
-func TestAlertDictionary(t *testing.T) {
-	p := payload.APS{Alert: payload.Alert{Title: "Message", Body: "Message received from Bob"}}
-	expected := []byte(`{"aps":{"alert":{"title":"Message","body":"Message received from Bob"}}}`)
-	testPayload(t, p, expected)
 }
