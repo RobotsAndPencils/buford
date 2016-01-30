533041670a2f84e819ca0bab39533b992a0f2e52
Nathan Youngman <git@nathany.com>
2016-01-29 17:29:44 -0700

    website push example: more endpoints

--- example/website/main.go
+++ example/website/main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/json"
+	"flag"
 	"html/template"
 	"io"
 	"log"
@@ -12,6 +13,8 @@ import (
 	"path/filepath"
 
 	"github.com/RobotsAndPencils/buford/certificate"
+	"github.com/RobotsAndPencils/buford/payload"
+	"github.com/RobotsAndPencils/buford/push"
 	"github.com/RobotsAndPencils/buford/pushpackage"
 )
 
@@ -25,11 +28,32 @@ var (
 		WebServiceURL:       "https://e2f9f182.ngrok.io",
 	}
 
-	templates = template.Must(template.ParseFiles("index.html"))
+	templates = template.Must(template.ParseFiles("index.html", "request.html"))
 )
 
-func requestPermissionHandler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "index.html", website)
+}
+
+func requestPermissionHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "request.html", website)
+}
+
+func pushHandler(w http.ResponseWriter, r *http.Request) {
+	p := payload.Browser{
+		Alert: payload.BrowserAlert{
+			Title: "Hello",
+			Body:  "Hello HTTP/2",
+		},
+		// URLArgs must match placeholders in URLFormatString
+		URLArgs: []string{"click", "hello"},
+	}
+
+	id, err := Service.Push(DeviceToken, nil, p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("apns-id:", id)
 }
 
 // MustOpen a file or fail.
@@ -93,16 +117,36 @@ func logHandler(w http.ResponseWriter, r *http.Request) {
 var (
 	Cert       *x509.Certificate
 	PrivateKey *rsa.PrivateKey
+
+	Service push.Service
+	// TODO: set this from endpoint for registering device
+	DeviceToken = "D23..."
 )
 
 func main() {
+	var filename, password string
+
+	flag.StringVar(&filename, "c", "", "Path to p12 certificate file")
+	flag.StringVar(&password, "p", "", "Password for p12 file.")
+	flag.Parse()
+
 	var err error
-	Cert, PrivateKey, err = certificate.Load("../../cert-website.p12", "")
+	Cert, PrivateKey, err = certificate.Load(filename, password)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", requestPermissionHandler)
+	Service = push.Service{
+		Client: push.NewClient(certificate.TLS(Cert, PrivateKey)),
+		Host:   push.Production,
+	}
+
+	// TODO: endpoints for registering/forgetting devices
+	// TODO: endpoint for clicks
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/request", requestPermissionHandler)
+	http.HandleFunc("/push", pushHandler)
 	http.HandleFunc("/v1/pushPackages/"+website.PushID, pushPackagesHandler)
 	http.HandleFunc("/v1/log", logHandler)
 	http.ListenAndServe(":5000", nil)
