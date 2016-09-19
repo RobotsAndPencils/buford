08161f85dfd3d6f74bf7d304abe10bf9c8dd14b7
Nathan Youngman <git@nathany.com>
2016-09-19 16:57:57 -0600

    jwt example, unfortunately not working
    > JWT authentication token is invalid

---
+++ example/jwt/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/RobotsAndPencils/buford/push"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func main() {
+	var deviceToken, filename, keyID, teamID, bundleID string
+
+	flag.StringVar(&deviceToken, "d", "", "Device token")
+	flag.StringVar(&filename, "k", "", "Path to private signing key")
+	flag.StringVar(&keyID, "kid", "", "Key ID")
+	flag.StringVar(&teamID, "t", "", "TeamID")
+	flag.StringVar(&bundleID, "b", "", "Bundle ID for app")
+	flag.Parse()
+
+	privateBytes, err := ioutil.ReadFile(filename)
+	exitOnError(err)
+
+	block, _ := pem.Decode(privateBytes)
+	if block == nil {
+		log.Fatal("Key file must be PEM encoded.")
+	}
+
+	privateKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	exitOnError(err)
+
+	// if pkey, ok := privateKey.(*ecdsa.PrivateKey); ok {
+	// 	log.Printf("%#v", pkey)
+	// }
+
+	// privateKey, err := jwt.ParseECPrivateKeyFromPEM(privateBytes)
+	// exitOnError(err)
+
+	service := push.NewService(http.DefaultClient, push.Development)
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
+		"iss": teamID,
+		"iat": time.Now().Unix(),
+	})
+	// delete(token.Header, "typ")
+	token.Header["kid"] = keyID
+	log.Printf("%#v\n", token)
+
+	// push the notification:
+	tokenString, err := token.SignedString(privateKey)
+	exitOnError(err)
+	log.Println(tokenString)
+
+	// _, err = jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+	// 	log.Printf("%#v\n", token)
+	// 	return publicKey, nil
+	// })
+	// exitOnError(err)
+
+	h := &push.Headers{Authorization: tokenString, Topic: bundleID}
+
+	id, err := service.Push(deviceToken, h, []byte(`{"aps":{"alert":"Hello HTTP/2"}}`))
+	exitOnError(err)
+
+	fmt.Println("apns-id:", id)
+}
+
+func exitOnError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
